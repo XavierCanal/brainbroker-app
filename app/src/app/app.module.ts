1d29b3d30378fa49990eca2b1bd36ffb57073ef0
XavierCanal <xcanal8@boscdelacoma.cat>
2023-05-23 16:52:23 +0200

    initial commit

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSelectModule} from "@angular/material/select";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {NgxMatSelectSearchModule} from "ngx-mat-select-search";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    NgxMatSelectSearchModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app/src/app/dashboard/dashboard/dashboard.component.ts
@@ -0,0 +1,110 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ReplaySubject, Subject} from "rxjs";
+import {FormControl} from "@angular/forms";
+import {MatSelect} from "@angular/material/select";
+import {take, takeUntil} from "rxjs/operators";
+
+interface Website {
+  id: string;
+  name: string;
+}
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  title = 'app-material3';
+
+  protected websites: Website[] = [
+    {id: '1', name: 'ItSolutionStuff.com'},
+    {id: '2', name: 'HDTuto.com'},
+    {id: '3', name: 'Nicesnippets.com'},
+    {id: '4', name: 'Google.com'},
+    {id: '5', name: 'laravel.com'},
+    {id: '6', name: 'npmjs.com'},
+  ];
+
+  public websiteCtrl: FormControl = new FormControl();
+  public websiteFilterCtrl: FormControl = new FormControl();
+  public filteredWebsites: ReplaySubject<any> = new ReplaySubject(1);
+
+
+  @ViewChild('singleSelect', { static: true }) singleSelect: MatSelect | undefined;
+  protected _onDestroy = new Subject();
+  constructor() { }
+
+  /**
+   * Write code on Method
+   *
+   * method logical code
+   */
+  ngOnInit() {
+    this.websiteCtrl.setValue(this.websites[1]);
+    this.filteredWebsites.next(this.websites.slice());
+
+    this.websiteFilterCtrl.valueChanges
+      .pipe(takeUntil(this._onDestroy))
+      .subscribe(() => {
+        this.filterBanks();
+      });
+  }
+
+  /**
+   * Write code on Method
+   *
+   * method logical code
+   */
+  ngAfterViewInit() {
+    this.setInitialValue();
+  }
+
+  /**
+   * Write code on Method
+   *
+   * method logical code
+   */
+  ngOnDestroy() {
+    this._onDestroy.next();
+    this._onDestroy.complete();
+  }
+
+  /**
+   * Write code on Method
+   *
+   * method logical code
+   */
+  protected setInitialValue() {
+    this.filteredWebsites
+      .pipe(take(1), takeUntil(this._onDestroy))
+      .subscribe(() => {
+        // @ts-ignore
+        this.singleSelect.compareWith = (a: Website, b: Website) => a && b && a.id === b.id;
+      });
+  }
+
+  /**
+   * Write code on Method
+   *
+   * method logical code
+   */
+  protected filterBanks() {
+    if (!this.websites) {
+      return;
+    }
+
+    let search = this.websiteFilterCtrl.value;
+    if (!search) {
+      this.filteredWebsites.next(this.websites.slice());
+      return;
+    } else {
+      search = search.toLowerCase();
+    }
+
+    this.filteredWebsites.next(
+      this.websites.filter(bank => bank.name.toLowerCase().indexOf(search) > -1)
+    );
+
+  }
+}
